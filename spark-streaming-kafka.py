196529e672651e769d1ad02ac630112835efd74d
rajchandak <raj.chandak@colorado.edu>
2021-04-27 16:32:05 -0600

    added mongo stuff

--- spark-streaming-kafka.py
+++ spark-streaming-kafka.py
@@ -1,81 +1,102 @@
-# from pyspark import SparkContext
+from pyspark import SparkContext
 from pyspark.streaming import StreamingContext
-# from pyspark.streaming.kafka import KafkaUtils
+from pyspark.streaming.kafka import KafkaUtils
+import json
+import pymongo
+import uuid
+import random
 
-# sc = SparkContext(appName="KafkaStream")
-# ssc = StreamingContext(sc,10)
-
-# message = KafkaUtils.createDirectStream(ssc,topics=["creditcard"],kafkaParams={"metadata.broker.list":"localhost:9092"})
-# message.pprint()
-
-# ssc.start()
-# ssc.awaitTermination()
-
-
-from pyspark.sql import SparkSession
-from pyspark.sql.functions import *
-from pyspark.sql.types import *
+mongo_client = pymongo.MongoClient("mongodb://localhost:27017/")
+db = mongo_client["spark"]
+collection = db["resultData"]
 
 
-# Spark session & context
-spark = (SparkSession
-         .builder
-         .master('local')
-         .appName('fraud-detection')
-         # Add kafka package
-         .config("spark.jars", "./spark-sql-kafka-0-10_2.12-3.1.1.jar, ./kafka-clients-2.8.0.jar")
-         .config("spark.executor.extraClassPath", "./spark-sql-kafka-0-10_2.12-3.1.1.jar, ./kafka-clients-2.8.0.jar")
-         .config("spark.executor.extraLibrary", "./spark-sql-kafka-0-10_2.12-3.1.1.jar, ./kafka-clients-2.8.0.jar")
-         .config("spark.driver.extraClassPath", "./spark-sql-kafka-0-10_2.12-3.1.1.jar, ./kafka-clients-2.8.0.jar")
-         .getOrCreate())
-sc = spark.sparkContext
+def printMessage(message):
+  for st in message.collect():
+    input_dict = json.loads(st[1])
+    id = uuid.uuid4()
+    input_dict["transaction_id"] = str(id)
+    print(input_dict)
+    result_dict = { "transaction_id": str(id), "result": random.randint(0,1) }
+    collection.insert_one(result_dict)
 
-df = (spark
-  .readStream
-  .format("kafka")
-  .option("kafka.bootstrap.servers", "localhost:9092") # kafka server
-  .option("subscribe", "creditcard") # topic
-  .load())
-# df1 = df.selectExpr("CAST(value AS STRING)")
+sc = SparkContext(appName="KafkaStream")
+ssc = StreamingContext(sc,10)
 
-mySchema = StructType()\
- .add("Time", IntegerType())\
- .add("V1", DoubleType())\
- .add("V2", DoubleType())\
- .add("V3", DoubleType())\
- .add("V4", DoubleType())\
- .add("V5", DoubleType())\
- .add("V6", DoubleType())\
- .add("V7", DoubleType())\
- .add("V8", DoubleType())\
- .add("V9", DoubleType())\
- .add("V10", DoubleType())\
- .add("V11", DoubleType())\
- .add("V12", DoubleType())\
- .add("V13", DoubleType())\
- .add("V14", DoubleType())\
- .add("V15", DoubleType())\
- .add("V16", DoubleType())\
- .add("V17", DoubleType())\
- .add("V18", DoubleType())\
- .add("V19", DoubleType())\
- .add("V20", DoubleType())\
- .add("V21", DoubleType())\
- .add("V22", DoubleType())\
- .add("V23", DoubleType())\
- .add("V24", DoubleType())\
- .add("V25", DoubleType())\
- .add("V26", DoubleType())\
- .add("V27", DoubleType())\
- .add("V28", DoubleType())\
- .add("Amount", DoubleType())\
- .add("Class", IntegerType())\
+message = KafkaUtils.createDirectStream(ssc,topics=["creditcard"],kafkaParams={"metadata.broker.list":"localhost:9092"})
+message.foreachRDD(printMessage)
 
-df1 = df.selectExpr("CAST(value AS STRING)", "CAST(timestamp AS TIMESTAMP)")\
-    .select(from_json(col("value"), mySchema).alias("data"), "timestamp")\
-    .select("data.*", "timestamp")
+ssc.start()
+ssc.awaitTermination()
 
-df1.printSchema()
+
+
+# from pyspark.sql import SparkSession
+# from pyspark.sql.functions import *
+# from pyspark.sql.types import *
+
+
+# # Spark session & context
+# spark = (SparkSession
+#          .builder
+#          .master('local')
+#          .appName('fraud-detection')
+#          # Add kafka package
+#          .config("spark.jars", "./spark-sql-kafka-0-10_2.12-3.1.1.jar, ./kafka-clients-2.8.0.jar")
+#          .config("spark.executor.extraClassPath", "./spark-sql-kafka-0-10_2.12-3.1.1.jar, ./kafka-clients-2.8.0.jar")
+#          .config("spark.executor.extraLibrary", "./spark-sql-kafka-0-10_2.12-3.1.1.jar, ./kafka-clients-2.8.0.jar")
+#          .config("spark.driver.extraClassPath", "./spark-sql-kafka-0-10_2.12-3.1.1.jar, ./kafka-clients-2.8.0.jar")
+#          .getOrCreate())
+# sc = spark.sparkContext
+
+
+# mySchema = StructType()\
+#  .add("Time", IntegerType())\
+#  .add("V1", StringType())\
+#  .add("V2", StringType())\
+#  .add("V3", StringType())\
+#  .add("V4", StringType())\
+#  .add("V5", StringType())\
+#  .add("V6", StringType())\
+#  .add("V7", StringType())\
+#  .add("V8", StringType())\
+#  .add("V9", StringType())\
+#  .add("V10", StringType())\
+#  .add("V11", StringType())\
+#  .add("V12", StringType())\
+#  .add("V13", StringType())\
+#  .add("V14", StringType())\
+#  .add("V15", StringType())\
+#  .add("V16", StringType())\
+#  .add("V17", StringType())\
+#  .add("V18", StringType())\
+#  .add("V19", StringType())\
+#  .add("V20", StringType())\
+#  .add("V21", StringType())\
+#  .add("V22", StringType())\
+#  .add("V23", StringType())\
+#  .add("V24", StringType())\
+#  .add("V25", StringType())\
+#  .add("V26", StringType())\
+#  .add("V27", StringType())\
+#  .add("V28", StringType())\
+#  .add("Amount", DoubleType())\
+#  .add("Class", IntegerType())\
+
+# df = (spark
+#   .readStream
+#   .format("kafka")
+#   .option("kafka.bootstrap.servers", "localhost:9092") # kafka server
+#   .option("subscribe", "creditcard") # topic
+#   .load())
+# # df1 = df.selectExpr("CAST(value AS STRING)")
+
+
+# df1 = df.selectExpr("CAST(value AS STRING)", "CAST(timestamp AS TIMESTAMP)")\
+#     .select(from_json(col("value"), mySchema).alias("data"), "timestamp")\
+#     .select("data.*", "timestamp")
+
+# df1.printSchema()
 
 # df1.writeStream\
 #     .trigger(processingTime="5 seconds")\
@@ -84,6 +105,6 @@ df1.printSchema()
 #     .start()\
 #     .awaitTermination()
 
-list_persons = map(lambda row: row.asDict(), df1.collect())
+# list_persons = map(lambda row: row.asDict(), df1.collect())
 
-print(list_persons)
+# print(list_persons)
