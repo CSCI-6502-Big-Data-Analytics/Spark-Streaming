b1b489da5e38cf47472cffbff8e2ae040d5ab311
rajchandak <raj.chandak@colorado.edu>
2021-04-28 02:11:23 -0600

    integrated with flask

--- spark-streaming-kafka.py
+++ spark-streaming-kafka.py
@@ -5,106 +5,63 @@ import json
 import pymongo
 import uuid
 import random
+import os, sys
+from pyspark.sql import SparkSession, Row
+from pyspark.ml import PipelineModel
+from pyspark.ml.classification import LogisticRegressionModel
+from pyspark.sql.types import FloatType
+import pyspark.sql.functions as f
+
+spark = SparkSession.builder.appName('fraud-detection').master("local[*]").getOrCreate()
+sc = spark.sparkContext
+ssc = StreamingContext(sc, 10)
+
+LR_MODEL_SAVEPATH = 'saves/LRBalancedModel'
+PIPELINE_SAVEPATH = 'saves/pipelineModelBalanced'
+
+pipelineModel = PipelineModel.load(PIPELINE_SAVEPATH)
+lrModel = LogisticRegressionModel.load(LR_MODEL_SAVEPATH)
+
+def predict(input_transaction_list, pipelineModel, lrModel, id_list):
+    
+    testDf = spark.createDataFrame([Row(**i) for i in input_transaction_list])
+    for col in testDf.columns:
+        testDf = testDf.withColumn(col, testDf[col].cast(FloatType()))
+    all_input_cols = testDf.columns
+    testDf = pipelineModel.transform(testDf)
+    selectedCols = ['features'] + all_input_cols 
+    testDf = testDf.select(selectedCols)    
+       
+    outputDf = lrModel.transform(testDf)
+    predictions = outputDf.select(f.collect_list('prediction')).first()[0]
+    print("\n=========================================================================\n")
+    for i in range(len(predictions)):
+      print(id_list[i], " : ", int(predictions[i]))
+      result_dict = { "transaction_id": str(id_list[i]), "result": int(predictions[i]) }
+      collection.insert_one(result_dict)
+    print("\n=========================================================================\n")
+
+def formatTransaction(transaction):
+  input_transaction_list, id_list = [], []
+  for st in transaction.collect():
+    transaction_dict = json.loads(st[1])
+    #transaction_dict["_c0"] = random.randint(0,1000)
+    transaction_dict["Time"] = int(transaction_dict["Time"])
+    transaction_dict["Amount"] = float(transaction_dict["Amount"])
+    del transaction_dict["Class"]
+    input_transaction_list.append(transaction_dict)
+    id_list.append(str(uuid.uuid4()))
+  if input_transaction_list:
+    predict(input_transaction_list, pipelineModel, lrModel, id_list)
+    
 
 mongo_client = pymongo.MongoClient("mongodb://localhost:27017/")
 db = mongo_client["spark"]
 collection = db["resultData"]
 
-
-def printMessage(message):
-  for st in message.collect():
-    input_dict = json.loads(st[1])
-    id = uuid.uuid4()
-    input_dict["transaction_id"] = str(id)
-    print(input_dict)
-    result_dict = { "transaction_id": str(id), "result": random.randint(0,1) }
-    collection.insert_one(result_dict)
-
-sc = SparkContext(appName="KafkaStream")
-ssc = StreamingContext(sc,10)
-
-message = KafkaUtils.createDirectStream(ssc,topics=["creditcard"],kafkaParams={"metadata.broker.list":"localhost:9092"})
-message.foreachRDD(printMessage)
+transactions = KafkaUtils.createDirectStream(ssc, topics=["creditcard"], kafkaParams={"metadata.broker.list":"localhost:9092"})
+if transactions:
+  transactions.foreachRDD(formatTransaction)
 
 ssc.start()
 ssc.awaitTermination()
-
-
-
-# from pyspark.sql import SparkSession
-# from pyspark.sql.functions import *
-# from pyspark.sql.types import *
-
-
-# # Spark session & context
-# spark = (SparkSession
-#          .builder
-#          .master('local')
-#          .appName('fraud-detection')
-#          # Add kafka package
-#          .config("spark.jars", "./spark-sql-kafka-0-10_2.12-3.1.1.jar, ./kafka-clients-2.8.0.jar")
-#          .config("spark.executor.extraClassPath", "./spark-sql-kafka-0-10_2.12-3.1.1.jar, ./kafka-clients-2.8.0.jar")
-#          .config("spark.executor.extraLibrary", "./spark-sql-kafka-0-10_2.12-3.1.1.jar, ./kafka-clients-2.8.0.jar")
-#          .config("spark.driver.extraClassPath", "./spark-sql-kafka-0-10_2.12-3.1.1.jar, ./kafka-clients-2.8.0.jar")
-#          .getOrCreate())
-# sc = spark.sparkContext
-
-
-# mySchema = StructType()\
-#  .add("Time", IntegerType())\
-#  .add("V1", StringType())\
-#  .add("V2", StringType())\
-#  .add("V3", StringType())\
-#  .add("V4", StringType())\
-#  .add("V5", StringType())\
-#  .add("V6", StringType())\
-#  .add("V7", StringType())\
-#  .add("V8", StringType())\
-#  .add("V9", StringType())\
-#  .add("V10", StringType())\
-#  .add("V11", StringType())\
-#  .add("V12", StringType())\
-#  .add("V13", StringType())\
-#  .add("V14", StringType())\
-#  .add("V15", StringType())\
-#  .add("V16", StringType())\
-#  .add("V17", StringType())\
-#  .add("V18", StringType())\
-#  .add("V19", StringType())\
-#  .add("V20", StringType())\
-#  .add("V21", StringType())\
-#  .add("V22", StringType())\
-#  .add("V23", StringType())\
-#  .add("V24", StringType())\
-#  .add("V25", StringType())\
-#  .add("V26", StringType())\
-#  .add("V27", StringType())\
-#  .add("V28", StringType())\
-#  .add("Amount", DoubleType())\
-#  .add("Class", IntegerType())\
-
-# df = (spark
-#   .readStream
-#   .format("kafka")
-#   .option("kafka.bootstrap.servers", "localhost:9092") # kafka server
-#   .option("subscribe", "creditcard") # topic
-#   .load())
-# # df1 = df.selectExpr("CAST(value AS STRING)")
-
-
-# df1 = df.selectExpr("CAST(value AS STRING)", "CAST(timestamp AS TIMESTAMP)")\
-#     .select(from_json(col("value"), mySchema).alias("data"), "timestamp")\
-#     .select("data.*", "timestamp")
-
-# df1.printSchema()
-
-# df1.writeStream\
-#     .trigger(processingTime="5 seconds")\
-#     .outputMode("update")\
-#     .format("console")\
-#     .start()\
-#     .awaitTermination()
-
-# list_persons = map(lambda row: row.asDict(), df1.collect())
-
-# print(list_persons)
